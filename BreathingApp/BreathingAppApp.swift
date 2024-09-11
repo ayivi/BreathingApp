d46131f6f588ac17837639bac380eb5140b285ca
ayivi <selormkwasi@gmail.com>
2024-09-11 20:56:47 +0000

    Initial Commit

---
+++ BreathingApp/BreathingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingAppApp.swift
+//  BreathingApp
+//
+//  Created by Ayivi on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BreathingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
